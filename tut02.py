f7262acc20b55e7bb1934198b3531d300e187c7b
Ryan Krauss <ryanwkrauss@gmail.com>
2016-05-30 14:41:26 -0500

    tut02

---
+++ tut02.py
@@ -0,0 +1,16 @@
+import cherrypy
+
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!, 0.0.0.0"
+
+    @cherrypy.expose
+    def generate(self):
+        return ''.join(random.sample(string.hexdigits, 8))
+
+i
+if __name__ == '__main__':
+    conf = {'server.socket_host': '0.0.0.0'} 
+    cherrypy.config.update(conf)
+    cherrypy.quickstart(HelloWorld())
