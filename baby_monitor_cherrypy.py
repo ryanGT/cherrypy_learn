687d6d4c7d274a9f855a0c50b6bf1ae8a23ef0d4
Ryan Krauss <ryanwkrauss@gmail.com>
2016-05-31 09:46:52 -0500

    baby monitor 20

--- baby_monitor_cherrypy.py
+++ baby_monitor_cherrypy.py
@@ -4,97 +4,106 @@ import string
 import cherrypy
 
 import time, os, glob
-foldername = time.strftime('%m_%d_%y')
+
+foldername = time.strftime('%m_%d_%y')# specify folder name based on
+                                      # date when script is run, mm_dd_yy
+                                      
 
 def image_name_to_time_stamp(relpath):
     """Convert an image file name to an easier to read time stamp
-    string"""
+    string; filenames use the format image_HH_MM_SS_PM.jpg"""
     fmt = 'image_%I_%M_%S_%p.jpg'
-    folder, fn = os.path.split(relpath)
-    time_struct = time.strptime(fn, fmt)
+    folder, fn = os.path.split(relpath)# break image path into folder
+                                       # and filename
+    time_struct = time.strptime(fn, fmt)# convert string to time variable
+    # convert time variable into a string with a different format
+    # (HH:MM:SS PM):
     time_stamp = time.strftime('%I:%M:%S %p', time_struct)
     return time_stamp
 
 
 class StringGenerator(object):
     def find_img_folder(self):
-        date_folder_rel = os.path.join('img',foldername)
+        """Find folder for current date in the img directory.  The
+        return string should be img/mm_dd_yy, for example img/05_31_16
+        for May 31, 2016"""
+        date_folder_rel = os.path.join('img',foldername)# foldername
+                                                        # is generated
+                                                        # above
 
-        if not os.path.exists(date_folder_rel):
-            print('problem with relpath for date folder: ' + date_folder_rel)
-        else:
-            return date_folder_rel
+        # throw an error if the date folder doesn't exist:
+        assert os.path.exists(date_folder_rel), "folder not found: " + \
+                   date_folder_rel
+
+        return date_folder_rel
+
 
     def find_images(self, date_folder):
+        """Find all image matching the pattern image_*.jpg in the
+        date_folder"""
         pat = os.path.join(date_folder, 'image_*.jpg')
         jpeg_list = glob.glob(pat)
         jpeg_list.sort()
         return jpeg_list
     
-        #        img_part = """<img src="/img/webtest.png" width=600px>
-
 
     @cherrypy.expose
-    def show_image(self, index=-1):
-        index = int(index)
+    def show_image(self, img_index=-1):
+        """Serve a webpage that displays date folder, filename, and
+        time stamp along with the latest image (or an older image if
+        img_index is not -1)"""
+        img_index = int(img_index)# if passed in using query string, this
+                          # would be a string; make sure it is an
+                          # integer
+                          
+        # html header string:
         header = """ <html>
         <head>
         <title>CherryPy Baby Monitor</title>
         </head>
         <html>
         <body>"""
-        date_folder = self.find_img_folder()
-        print('date_folder: ' + date_folder)
-        jpeg_list = self.find_images(date_folder)
-        jpeg_relpath = jpeg_list[index]
-        top_part = "filename: %s" % jpeg_relpath
+        date_folder = self.find_img_folder()# get date folder relative path
+        jpeg_list = self.find_images(date_folder)# find all images in
+                                                 # date folder
+        jpeg_relpath = jpeg_list[img_index]# get specific image specified
+                                           # by img_index; -1 is the last one
+                                           # (most recent)
+
+        # build the text part of the webpage to be displayed above the
+        # picture:
+        top_part = "date folder: %s" % date_folder
+        # note that html uses <br> for break or linefeed or newline:
+        top_part += " <br> filename: %s" % jpeg_relpath
+        # get easy to read time stamp from file name
         time_stamp = image_name_to_time_stamp(jpeg_relpath)
         top_part += " <br> time stamp: %s" % time_stamp
-        jpeg_path = '/' + jpeg_relpath
-        print('jpeg_path: ' + jpeg_path)
+        jpeg_path = '/' + jpeg_relpath# cherrpy requires a leading /
+                                      # on relative paths
+        # html code to display the image:
         img_part = '<img src="%s" width=600px>' % jpeg_path
+        # closing html code:
         footer = """</body>
         </html>"""
+        # join strings together with <br> in between parts:
         out = " <br> ".join([header, top_part, img_part])
         return out
 
         
     @cherrypy.expose
     def index(self):
-        return "Hello world!"
+        #return "Hello world!"
+        raise cherrypy.HTTPRedirect("/show_image")
 
     @cherrypy.expose
     def generate(self, length=8):
         return ''.join(random.sample(string.hexdigits, int(length)))
 
 
-    @cherrypy.expose
-    def myfunc(self, a=1, b=2):
-        a = float(a)
-        b = float(b)
-        c = a + b
-        line1 = 'a = %s' % a
-        line2 = 'b = %s' % b
-        line3 = 'c = %s' % c
-        mylist = [line1, line2, line3]
-        outstr = ' <br> '.join(mylist)
-        return outstr
-
-
 if __name__ == '__main__':
-    ## img_key = '/' + date_folder
-    ## img_rel = '.' + img_key
-    ## removed from conf:
-    ## img_key: {
-    ## "tools.staticdir.on": True,
-    ## "tools.staticdir.dir": img_rel,
-    ## }, \
-
     conf = {'/': {
                 'tools.sessions.on': True, \
                 'tools.staticdir.root': os.path.abspath(os.getcwd()), \
-                'tools.staticdir.debug': True, \
-                'log.screen': True
                 }, \
             '/static': {
                 'tools.staticdir.on': True,
