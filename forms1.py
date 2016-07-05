efbae47b460dc0ac3e7dace39a34ac17c606900b
Ryan Krauss <ryanwkrauss@gmail.com>
2016-07-05 12:32:24 -0400

    forms1 attempt 1

---
+++ forms1.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+import cherrypy
+
+class StringGenerator(object):
+    @cherrypy.expose
+    def index(self):
+        return """<html>
+            <head></head>
+            <body>
+                <form method="get" action="respond">
+                    <input type="text" value="8" name="text_input" />
+                    <button type="submit">Submit</button>
+                </form>
+            </body>
+        </html>"""
+
+    @cherrypy.expose
+    def respond(self, text_input='hello'):
+        msg = 'You sent me this text_input: %s' % text_input
+        return msg
+
+
+if __name__ == '__main__':
+    conf = {'server.socket_host': '0.0.0.0'} 
+    cherrypy.config.update(conf)
+    cherrypy.quickstart(StringGenerator(),conf=conf)
--- oop_intro.py
+++ oop_intro.py
@@ -1,29 +1,39 @@
 class myclass(object):
     def __init__(self, a, b=7):
+        print('calling myclass.__init__')
         self.a = a
         self.b = b
 
 
     def calc_c(self):
+        print('in myclass.calc_c')
         self.c = self.a + self.b
         return self.c
 
 
     def calc_d(self):
+        print('in myclass.calc_d')
         self.d = self.a * self.b
         return self.d
     
 
-class derived_class(myobject):
+class derived_class(myclass):
     def __init__(self, a, b=10):
-        myobject.__init__(self, a, b=b)
+        print('calling derived_class.__init__')
+        myclass.__init__(self, a, b=b)
 
 
     def calc_d(self):
+        print('in derived_class.calc_d')
         self.d = float(self.a) / float(self.b)
         return self.d
 
 
 
 jeff = myclass(3,8)
+jeff.calc_c()
+jeff.calc_d()
+print('='*20)
 bob = derived_class(4)
+bob.calc_c()
+bob.calc_d()
